9b1a38b6ca0a26bd5d968be1b3cd1753e309a51b
Naomi Bächler <Theera4th@gmail.com>
2026-07-30 03:35:48 +0200

    alpha 30-07-26 Page layout

--- glp21/src/app/Balancieren/page.jsx
+++ glp21/src/app/Balancieren/page.jsx
@@ -5,103 +5,125 @@ import React, { useState } from 'react';
 
 function page() {
 
-  const [expandedHobby, setExpandedHobby] = useState(null);
+  const [expandedStep1, setExpandedStep1] = useState(null);
+  const [expandedStep2, setExpandedStep2] = useState(null);
+  const [expandedStep3, setExpandedStep3] = useState(null);
+  const [expandedStep4, setExpandedStep4] = useState(null);
 
-  const handleToggle = (alt) => {
-    setExpandedHobby(prev => (prev === alt ? null : alt));
+  const handleToggle1 = (alt) => {
+    setExpandedStep1(prev => (prev === alt ? null : alt));
+  };
+  const handleToggle2 = (alt) => {
+    setExpandedStep2(prev => (prev === alt ? null : alt));
+  };
+  const handleToggle3 = (alt) => {
+    setExpandedStep3(prev => (prev === alt ? null : alt));
+  };
+  const handleToggle4 = (alt) => {
+    setExpandedStep4(prev => (prev === alt ? null : alt));
   };
 
   const step1 = [
-    { src: "/vids/Balacieren/Station1einfach.mov", alt: "Übung Einfach", desc: "Übung Einfach", resc: "Übung Einfach Untertitel" },
-    { src: "/vids/Balacieren/Station1mittel.mov", alt: "Übung Mittel", desc: "Übung Mittel", resc: "Übung Mittel Untertitel" },
-    { src: "/vids/Balacieren/Station1schwer.mov", alt: "Übung Schwer", desc: "Übung Schwer", resc: "Übung Schwer Untertitel" },
-    { src: "/vids/Balacieren/Station1bild.jpg", alt: "Übung Erklärung", desc: "Übung Erklärung", resc: "Übung Erklärung Untertitel" }
+    { src: "/vids/Balacieren/Schritt1einfach.mov", alt: "Übung Einfach", desc: "Darüber laufen vor- und rückwärts", resc: "? Untertitel" },
+    { src: "/vids/Balacieren/Schritt1mittel.mov", alt: "Übung Mittel", desc: "Springen mit Standwaagen", resc: "? Untertitel" },
+    { src: "/vids/Balacieren/Schritt1schwer.mov", alt: "Übung Schwer", desc: "Handstand an der Sprossenwand", resc: "? Untertitel" },
+    { src: "", alt: "Erklärungen", desc: "Erklärungen", resc: "Damit die Motivation vorhanden bleibt, können die Seile selber in Formen gelegt werden und zusätzliche ergänzt werden." }
   ];
 
   const step2 = [
-    { src: "/vids/Balacieren/Station2einfach.mov", alt: "Übung Einfach", desc: "Übung Einfach", resc: "Übung Einfach Untertitel" },
-    { src: "/vids/Balacieren/Station2mittel.mov", alt: "Übung Mittel", desc: "Übung Mittel", resc: "Übung Mittel Untertitel" },
-    { src: "/vids/Balacieren/Station2schwer.mov", alt: "Übung Schwer", desc: "Übung Schwer", resc: "Übung Schwer Untertitel" },
-    { src: "/vids/Balacieren/Station2erkl.mov", alt: "Übung Erklärung", desc: "Übung Erklärung", resc: "Übung Erklärung Untertitel" }
+    { src: "/vids/Balacieren/Schritt2einfach.mov", alt: "Übung Einfach", desc: "Darüber laufen vor- und rückwärts", resc: "Darüberlaufen vor- und rückwärts" },
+    { src: "/vids/Balacieren/Schritt2mittel.mov", alt: "Übung Mittel", desc: "Halbe und ganze Drehungen", resc: "Halbe und ganze Drehungen" },
+    { src: "/vids/Balacieren/Schritt2schwer.mov", alt: "Übung Schwer", desc: "Standwaage", resc: "Standwaage" },
+    { src: "/vids/Balacieren/Schritt2erkl.mov", alt: "Erklärungen", desc: "Erklärungen", resc: "Hilfsgriffe: Bei der Standwaage sollte der Stützgriff verwendet werden und möglichst nahe am Körperschwerpunkt gehalten werden. Kinder können sich auch selber helfen, indem sie sich die Hand geben und so dem Gleichgewicht helfen." }
   ];
 
   const step3 = [
-    { src: "/vids/Balacieren/Station3einfach.mov", alt: "Übung Einfach", desc: "Übung Einfach", resc: "Übung Einfach Untertitel" },
-    { src: "/vids/Balacieren/Station3mittel.mov", alt: "Übung Mittel", desc: "Übung Mittel", resc: "Übung Mittel Untertitel" },
-    { src: "/vids/Balacieren/Station3schwer.mov", alt: "Übung Schwer", desc: "Übung Schwer", resc: "Übung Schwer Untertitel" },
-    { src: "/vids/Balacieren/Station3bild.JPEG", alt: "Übung Erklärung", desc: "Übung Erklärung", resc: "Übung Erklärung Untertitel" }
+    { src: "/vids/Balacieren/Schritt3einfach.mov", alt: "Übung Einfach", desc: "Darüber laufen", resc: "Darüber laufen" },
+    { src: "/vids/Balacieren/Schritt3mittel.mov", alt: "Übung Mittel", desc: "Seitwärts darüber laufen", resc: "Seitwärts darüber laufen" },
+    { src: "/vids/Balacieren/Schritt3schwer.mov", alt: "Übung Schwer", desc: "Standwaage", resc: "Standwaage" },
+    { src: "", alt: "Erklärungen", desc: "Erklärungen", resc: "Wenn die Kinder sehr unsicher sind, kann der Barren zu Beginn auch geradegestellt werden." }
   ];
 
   const step4 = [
-    { src: "/vids/Balacieren/Station4einfach.mov", alt: "Übung Einfach", desc: "Übung Einfach", resc: "Übung Einfach Untertitel" },
-    { src: "/vids/Balacieren/Station4mittel.mov", alt: "Übung Mittel", desc: "Übung Mittel", resc: "Übung Mittel Untertitel" },
-    { src: "/vids/Balacieren/Station4schwer.mov", alt: "Übung Schwer", desc: "Übung Schwer", resc: "Übung Schwer Untertitel" },
-    { src: "/vids/Balacieren/Station4bild.JPEG", alt: "Übung Erklärung", desc: "Übung Erklärung", resc: "Übung Erklärung Untertitel" }
+    { src: "/vids/Balacieren/Schritt4einfach.mov", alt: "Übung Einfach", desc: "Darüber laufen", resc: "Darüber laufen" },
+    { src: "/vids/Balacieren/Schritt4mittel.mov", alt: "Übung Mittel", desc: "Auf den Knien darüber und evtl. Jemanden kreuzen", resc: "Auf den Knien darüber und evtl. Jemanden kreuzen" },
+    { src: "/vids/Balacieren/Schritt4schwer.mov", alt: "Übung Schwer", desc: "Kreativ mit Bällen", resc: "Kreativ mit Bällen" },
+    { src: "", alt: "Erklärungen", desc: "Erklärungen", resc: "Bei Bedarf können die Bänkli zu Beginn auch andersrum stehen." }
   ];
 
   return (
     <>
+      {/* Bahnenbild */}
       <div className="flex-container">
-        <div className="abme">
-          <div style={{ whiteSpace: "pre-line", fontSize: "18px", display: "flex", flexDirection: "column", alignItems: "center" }}>
-            <h2>Bahnenbild</h2>
-            <a href="/vids/Balacieren/GanzeBahn.jpg" target="_blank">
-              <img src="/vids/Balacieren/GanzeBahn.jpg" alt="Gesamte Bahn" />
-            </a>
-          </div>
-          <div style={{ whiteSpace: "pre-line", fontSize: "18px", display: "flex", flexDirection: "row", justifyContent: "center" }}>
-            <p style={{ margin: "0 2.75rem" }}>Station 1</p>
-            <p style={{ margin: "0 2.75rem" }}>Station 2</p>
-            <p style={{ margin: "0 2.75rem" }}>Station 3</p>
-            <p style={{ margin: "0 2.75rem" }}>Station 4</p>
+        <div className="ame">
+          <h2>Bahnenbild</h2>
+          <a href="/vids/Balacieren/GanzeBahn.jpg" target="_blank">
+            <img src="/vids/Balacieren/GanzeBahn.jpg" alt="Gesamte Bahn" />
+          </a>
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            <p style={{ margin: "0 2.75rem" }}>Station 1</p> <p style={{ margin: "0 2.75rem" }}>Station 2</p> <p style={{ margin: "0 2.75rem" }}>Station 3</p> <p style={{ margin: "0 2.75rem" }}>Station 4</p>
           </div>
         </div>
       </div>
+      {/* Infotexts */}
       <div className="flex-container">
         <div className="abme">
-          <h2>InfoText: Achtungen</h2>
+          <h2>Informationen zur Bahn</h2>
           <div style={{ whiteSpace: "pre-line", fontSize: "18px" }}>
-            Infos zum Ablauf / zur Bahn / sicherheitvorschriften
+            Die Gerätebahn gehört zu der Kategorie «Balancieren» im Lehrplan 21 und deckt diese Kompetenzbereiche ab.<br />
+            <br /><b style={{ color: '#dcc' }}>Sicherheitshinweis:</b><br />
+            Wichtig zu beachten ist, dass die Sprossenwand gut befestig ist. Auch beim Reck und Barren sollte eine erwachsene Person kontrollieren, ob alles fest ist.
           </div>
         </div>
         <div className="abme">
-          <h2>InfoText: Material</h2>
+          <h2>Materialliste:</h2>
           <div style={{ whiteSpace: "pre-line", fontSize: "18px" }}>
-            Materialliste & Anwendungsfall
+            <b>Posten 1:</b> <br />
+            <li style={{ marginLeft: '24px' }}>Sprossenwand</li>
+            <li style={{ marginLeft: '24px' }}>2x dünne Matten (Leichtturnmatten)</li>
+            <li style={{ marginLeft: '24px' }}>1x 16er-Matte (Niedersprungmatte)</li>
+            <li style={{ marginLeft: '24px' }}>3x - 4x Sprungseile</li>
+            <b>Posten 2:</b> <br />
+            <li style={{ marginLeft: '24px' }}>1x tiefes Reck</li>
+            <li style={{ marginLeft: '24px' }}>2x dünne Matten (Leitturnmatten)</li>
+            <b>Posten 3:</b> <br />
+            <li style={{ marginLeft: '24px' }}>1x Schulstufenbarren</li>
+            <li style={{ marginLeft: '24px' }}>3x dünne Matten (Leitturnmatten)</li>
+            <b>Posten 4:</b> <br />
+            <li style={{ marginLeft: '24px' }}>2x Bänkli</li>
           </div>
         </div>
       </div>
       {/*Station 1*/}
       <details>
-        <summary>Station 1</summary>
+        <summary style={{ fontSize: 'x-Large' }}>Übungen Station 1</summary>
         <div className="flex-container">
           <div className="HnI" >
-            <h2>Übungen Station 1</h2>
+            <h2>Balancieren mit Seilen</h2>
             <div style={{ whiteSpace: "pre-line", fontSize: "18px", display: "flex", flexDirection: "column", alignItems: "center" }}>
-              <img src="/vids/Balacieren/Station1bild.jpg" alt="Station 1" />
-              <h4>InfoText</h4>
+              <img src="/vids/Balacieren/Schritt1bild.jpg" alt="Station 1" />
             </div>
             <br />
             <div className="pics">
-              {step1.map((hobby) => (
-                <div className="responsive" key={hobby.alt} onClick={() => handleToggle(hobby.alt)}>
+              {step1.map((step1) => (
+                <div className="responsive" key={step1.alt} onClick={() => handleToggle1(step1.alt)}>
                   <div className="gallery">
-                    {expandedHobby === hobby.alt ? (
+                    {expandedStep1 === step1.alt ? (
                       <>
-                        <a style={{ pointerEvents: "none", cursor: "default" }} href={hobby.src}>
+                        <a style={{ pointerEvents: "none", cursor: "default" }} href={step1.src}>
                           <Image
-                            src={hobby.src}
-                            alt={hobby.alt}
+                            src={step1.src}
+                            alt={step1.alt}
                             width={300}
                             height={200}
                             layout="responsive"
                             quality={75}
                           />
                         </a>
-                        <div className="desc">{hobby.resc}</div>
+                        <div className="desc">{step1.resc}</div>
                       </>
                     ) : (
-                      <div className="desc">{hobby.desc}</div>
+                      <div className="desc">{step1.desc}</div>
                     )}
                   </div>
                 </div>
@@ -112,35 +134,35 @@ function page() {
       </details>
       {/*Station 2*/}
       <details>
-        <summary>Station 2</summary>
+        <summary style={{ fontSize: 'x-Large' }}>Übungen Station 2</summary>
         <div className="flex-container">
           <div className="HnI" >
-            <h2>Übungen Station 2</h2>
+            <h2>Tiefes Reck</h2>
             <div style={{ whiteSpace: "pre-line", fontSize: "18px", display: "flex", flexDirection: "column", alignItems: "center" }}>
               <img src="/vids/Balacieren/Station2bild.jpeg" alt="Station 2" />
               <h4>InfoText</h4>
             </div>
             <br />
             <div className="pics">
-              {step2.map((hobby) => (
-                <div className="responsive" key={hobby.alt} onClick={() => handleToggle(hobby.alt)}>
+              {step2.map((step2) => (
+                <div className="responsive" key={step2.alt} onClick={() => handleToggle2(step2.alt)}>
                   <div className="gallery">
-                    {expandedHobby === hobby.alt ? (
+                    {expandedStep2 === step2.alt ? (
                       <>
-                        <a style={{ pointerEvents: "none", cursor: "default" }} href={hobby.src}>
+                        <a style={{ pointerEvents: "none", cursor: "default" }} href={step2.src}>
                           <Image
-                            src={hobby.src}
-                            alt={hobby.alt}
+                            src={step2.src}
+                            alt={step2.alt}
                             width={300}
                             height={200}
                             layout="responsive"
                             quality={75}
                           />
                         </a>
-                        <div className="desc">{hobby.resc}</div>
+                        <div className="desc">{step2.resc}</div>
                       </>
                     ) : (
-                      <div className="desc">{hobby.desc}</div>
+                      <div className="desc">{step2.desc}</div>
                     )}
                   </div>
                 </div>
@@ -151,35 +173,35 @@ function page() {
       </details>
       {/*Station 3*/}
       <details>
-        <summary>Station 3</summary>
+        <summary style={{ fontSize: 'x-Large' }}>Übungen Station 3</summary>
         <div className="flex-container">
           <div className="HnI" >
-            <h2>Übungen Station 3</h2>
+            <h2>Schräger Schulstufenbarren</h2>
             <div style={{ whiteSpace: "pre-line", fontSize: "18px", display: "flex", flexDirection: "column", alignItems: "center" }}>
               <img src="/vids/Balacieren/Station3bild.jpeg" alt="Station 3" />
               <h4>InfoText</h4>
             </div>
             <br />
             <div className="pics">
-              {step3.map((hobby) => (
-                <div className="responsive" key={hobby.alt} onClick={() => handleToggle(hobby.alt)}>
+              {step3.map((step3) => (
+                <div className="responsive" key={step3.alt} onClick={() => handleToggle3(step3.alt)}>
                   <div className="gallery">
-                    {expandedHobby === hobby.alt ? (
+                    {expandedStep3 === step3.alt ? (
                       <>
-                        <a style={{ pointerEvents: "none", cursor: "default" }} href={hobby.src}>
+                        <a style={{ pointerEvents: "none", cursor: "default" }} href={step3.src}>
                           <Image
-                            src={hobby.src}
-                            alt={hobby.alt}
+                            src={step3.src}
+                            alt={step3.alt}
                             width={300}
                             height={200}
                             layout="responsive"
                             quality={75}
                           />
                         </a>
-                        <div className="desc">{hobby.resc}</div>
+                        <div className="desc">{step3.resc}</div>
                       </>
                     ) : (
-                      <div className="desc">{hobby.desc}</div>
+                      <div className="desc">{step3.desc}</div>
                     )}
                   </div>
                 </div>
@@ -190,35 +212,35 @@ function page() {
       </details>
       {/*Station 4*/}
       <details>
-        <summary>Station 4</summary>
+        <summary style={{ fontSize: 'x-Large' }}>Übungen Station 4</summary>
         <div className="flex-container">
           <div className="HnI" >
-            <h2>Übungen Station 4</h2>
+            <h2>Bänklibalken</h2>
             <div style={{ whiteSpace: "pre-line", fontSize: "18px", display: "flex", flexDirection: "column", alignItems: "center" }}>
               <img src="/vids/Balacieren/Station4bild.jpeg" alt="Station 4" />
               <h4>InfoText</h4>
             </div>
             <br />
             <div className="pics">
-              {step4.map((hobby) => (
-                <div className="responsive" key={hobby.alt} onClick={() => handleToggle(hobby.alt)}>
+              {step4.map((step4) => (
+                <div className="responsive" key={step4.alt} onClick={() => handleToggle4(step4.alt)}>
                   <div className="gallery">
-                    {expandedHobby === hobby.alt ? (
+                    {expandedStep4 === step4.alt ? (
                       <>
-                        <a style={{ pointerEvents: "none", cursor: "default" }} href={hobby.src}>
+                        <a style={{ pointerEvents: "none", cursor: "default" }} href={step4.src}>
                           <Image
-                            src={hobby.src}
-                            alt={hobby.alt}
+                            src={step4.src}
+                            alt={step4.alt}
                             width={300}
                             height={200}
                             layout="responsive"
                             quality={75}
                           />
                         </a>
-                        <div className="desc">{hobby.resc}</div>
+                        <div className="desc">{step4.resc}</div>
                       </>
                     ) : (
-                      <div className="desc">{hobby.desc}</div>
+                      <div className="desc">{step4.desc}</div>
                     )}
                   </div>
                 </div>
