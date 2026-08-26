b6a09eedc5dd2ef30a64c04d6316869c21ddc38d
Sof-145 <sophie.hafner@students.lerbermatt.ch>
2026-08-26 20:51:03 +0200

    posten zu station

--- glp21/src/app/Beweglichkeit-und-Kraft/page.jsx
+++ glp21/src/app/Beweglichkeit-und-Kraft/page.jsx
@@ -61,7 +61,7 @@ function page() {
             <img src="/vids/BeweglichkeitKraft/GanzeBahn.jpeg" alt="Gesamte Bahn" />
           </a>
           <div style={{ display: "flex", flexDirection: "row" }}>
-            <p style={{ margin: "0 2.75rem" }}>Posten 1</p> <p style={{ margin: "0 2.75rem" }}>Posten 2</p> <p style={{ margin: "0 2.75rem" }}>Posten 3</p> <p style={{ margin: "0 2.75rem" }}>Posten 4</p>
+            <p style={{ margin: "0 2.75rem" }}>Station 1</p> <p style={{ margin: "0 2.75rem" }}>Station 2</p> <p style={{ margin: "0 2.75rem" }}>Station 3</p> <p style={{ margin: "0 2.75rem" }}>Station 4</p>
           </div>
         </div>
       </div>
@@ -96,7 +96,7 @@ function page() {
       </div>
       {/*Posten 1*/}
       <details>
-        <summary style={{ fontSize: 'x-Large' }}>Übungen Posten 1</summary>
+        <summary style={{ fontSize: 'x-Large' }}>Übungen Station 1</summary>
         <div className="flex-container">
           <div className="HnI" >
             <h2>Boden</h2>
@@ -134,7 +134,7 @@ function page() {
       </details>
       {/*Posten 2*/}
       <details>
-        <summary style={{ fontSize: 'x-Large' }}>Übungen Posten 2</summary>
+        <summary style={{ fontSize: 'x-Large' }}>Übungen Station 2</summary>
         <div className="flex-container">
           <div className="HnI" >
             <h2>Reck</h2>
@@ -172,7 +172,7 @@ function page() {
       </details>
       {/*Posten 3*/}
       <details>
-        <summary style={{ fontSize: 'x-Large' }}>Übungen Posten 3</summary>
+        <summary style={{ fontSize: 'x-Large' }}>Übungen Station 3</summary>
         <div className="flex-container">
           <div className="HnI" >
             <h2>Sprossenwand</h2>
@@ -210,7 +210,7 @@ function page() {
       </details>
       {/*Posten 4*/}
       <details>
-        <summary style={{ fontSize: 'x-Large' }}>Übungen Posten 4</summary>
+        <summary style={{ fontSize: 'x-Large' }}>Übungen Station 4</summary>
         <div className="flex-container">
           <div className="HnI" >
             <h2>Sprung mit einem Kasten</h2>
--- glp21/src/app/Helfen-Sichern-Kooperieren/page.jsx
+++ glp21/src/app/Helfen-Sichern-Kooperieren/page.jsx
@@ -61,7 +61,7 @@ function page() {
             <img src="/vids/HelfenSichern/GanzeBahn.jpeg" alt="Gesamte Bahn" />
           </a>
           <div style={{ display: "flex", flexDirection: "row" }}>
-            <p style={{ margin: "0 2.75rem" }}>Posten 1</p> <p style={{ margin: "0 2.75rem" }}>Posten 2</p> <p style={{ margin: "0 2.75rem" }}>Posten 3</p> <p style={{ margin: "0 2.75rem" }}>Posten 4</p>
+            <p style={{ margin: "0 2.75rem" }}>Station 1</p> <p style={{ margin: "0 2.75rem" }}>Station 2</p> <p style={{ margin: "0 2.75rem" }}>Station 3</p> <p style={{ margin: "0 2.75rem" }}>Station 4</p>
           </div>
         </div>
       </div>
@@ -95,7 +95,7 @@ function page() {
       </div>
       {/*Posten 1*/}
       <details>
-        <summary style={{ fontSize: 'x-Large' }}>Übungen Posten 1</summary>
+        <summary style={{ fontSize: 'x-Large' }}>Übungen Station 1</summary>
         <div className="flex-container">
           <div className="HnI" >
             <h2>Boden</h2>
@@ -133,7 +133,7 @@ function page() {
       </details>
       {/*Posten 2*/}
       <details>
-        <summary style={{ fontSize: 'x-Large' }}>Übungen Posten 2</summary>
+        <summary style={{ fontSize: 'x-Large' }}>Übungen Station 2</summary>
         <div className="flex-container">
           <div className="HnI" >
             <h2>Reck</h2>
@@ -171,7 +171,7 @@ function page() {
       </details>
       {/*Posten 3*/}
       <details>
-        <summary style={{ fontSize: 'x-Large' }}>Übungen Posten 3</summary>
+        <summary style={{ fontSize: 'x-Large' }}>Übungen Station 3</summary>
         <div className="flex-container">
           <div className="HnI" >
             <h2>Boden mit einer Sprossenwand</h2>
@@ -209,7 +209,7 @@ function page() {
       </details>
       {/*Posten 4*/}
       <details>
-        <summary style={{ fontSize: 'x-Large' }}>Übungen Posten 4</summary>
+        <summary style={{ fontSize: 'x-Large' }}>Übungen Station 4</summary>
         <div className="flex-container">
           <div className="HnI" >
             <h2>Schulstufenbarren</h2>
